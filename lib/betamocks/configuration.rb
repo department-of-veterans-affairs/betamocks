d90e6af37e1e816acfccefbc4a5517b594890cad
alastair <alastair.j.dawson@gmail.com>
2017-11-17 10:29:05 -0800

    update matches_path regex for wildcards

--- lib/betamocks/configuration.rb
+++ lib/betamocks/configuration.rb
@@ -51,7 +51,7 @@ module Betamocks
     end
 
     def matches_path(endpoint, method, path)
-      /#{endpoint[:path].gsub('/', '\/').gsub('*', '.*')}/ =~ path && endpoint[:method] == method
+      /\A#{endpoint[:path].gsub('/', '\/').gsub('*', '[^\/]*')}\Z/ =~ path && endpoint[:method] == method
     end
   end
 end
